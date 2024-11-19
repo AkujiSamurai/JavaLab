94dc19ab0cfd2e4f1965e55ded8e5338dfedfae7
Руслан <shcherbakov.rd@reliab.tech>
2024-11-19 17:03:23 +0300

    Lab2

--- src/main/java/tech/reliab/course/shcherbakov/ShcherbakovLab.java
+++ src/main/java/tech/reliab/course/shcherbakov/ShcherbakovLab.java
@@ -36,5 +36,30 @@ public class ShcherbakovLab {
         System.out.println(bankAtm);
         System.out.println(paymentAccount);
         System.out.println(creditAccount);
+
+        User user2 = userService.createUser("Иванов Иван Иванович", LocalDate.now(), "Программист");
+
+        Bank bank2 = bankService.createBank("ВТБ");
+
+        BankOffice bankOffice2 = bankOfficeService.createBankOffice("Офис", "Белгород", true, true, true, true, 100, bank2);
+
+        Employee employee2 = employeeService.createEmployee("Иванов Алексей Юрьевич", LocalDate.now(), "Аналитик", bank2, true, bankOffice2, true, 0);
+
+        BankAtm bankAtm2 = atmService.createBankAtm("Банкомат", "Белгород", bank2, bankOffice2, employee2, true, true, 100);
+
+        PaymentAccount paymentAccount2 = paymentAccountService.createPaymentAccount(user2, bank2);
+
+        CreditAccount creditAccount2 = creditAccountService.createCreditAccount(user2, bank2, LocalDate.now(), 10, 1000, 10, employee2, paymentAccount2);
+
+        System.out.println(user2);
+        System.out.println(bank2);
+        System.out.println(bankOffice2);
+        System.out.println(employee2);
+        System.out.println(bankAtm2);
+        System.out.println(paymentAccount2);
+        System.out.println(creditAccount2);
+
+
+
     }
 }
